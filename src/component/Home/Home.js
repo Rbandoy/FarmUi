df8125e5fb1f204c7fcb832212b92b0bfd666dbd
Rene Boy Andoy <45546823+Rbandoy@users.noreply.github.com>
2022-09-12 09:30:59 +0800

    Update Home.js

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -16,7 +16,7 @@ function Home() {
   useEffect(() => { 
     var config = {
       method: 'get',
-      url: ReactSession.get("user")?.role == undefined ? `https://rbandoy.site/post/postList/${post_id != undefined ? post_id : ""}` : ReactSession.get("user")?.role == 'admin' ? `https://rbandoy.site/post/postListApproval/${post_id != undefined ? post_id : ""}`:`https://rbandoy.site/post/postList/${post_id != undefined ? post_id : ""}`,
+      url: ReactSession.get("user")?.role == undefined ? `https://backend.agriweb.site/post/postList/${post_id != undefined ? post_id : ""}` : ReactSession.get("user")?.role == 'admin' ? `https://backend.agriweb.site/post/postListApproval/${post_id != undefined ? post_id : ""}`:`https://backend.agriweb.site/post/postList/${post_id != undefined ? post_id : ""}`,
       headers: { }
     };
     
